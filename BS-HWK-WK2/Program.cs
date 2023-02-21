a765db47d0a3f922dde002cf0d7717a9f3b6d20d
NickLu <word4205@gmail.com>
2023-02-21 10:47:48 +0800

    加入專案檔案。

---
+++ BS-HWK-WK2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS_HWK_WK2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < i + 1; j++)
+                {
+                    Console.Write(n - i);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
