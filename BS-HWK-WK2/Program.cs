863b8138b4818bb43a0a2b51fda23170c8e14b35
NickLu <word4205@gmail.com>
2023-02-21 10:48:48 +0800

    非LINQ三角形

--- BS-HWK-WK2/Program.cs
+++ BS-HWK-WK2/Program.cs
@@ -12,7 +12,8 @@ namespace BS_HWK_WK2
         {
             int n = 5;
 
-            for(int i = 0; i < n; i++)
+            // 非LINQ三角形
+            for (int i = 0; i < n; i++)
             {
                 for(int j = 0; j < i + 1; j++)
                 {
