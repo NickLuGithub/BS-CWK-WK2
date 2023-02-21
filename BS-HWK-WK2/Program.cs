17049f516af1c26198895d1f6046057fb0e3fdc6
NickLu <word4205@gmail.com>
2023-02-21 11:31:21 +0800

    完成LINQ版

--- BS-HWK-WK2/Program.cs
+++ BS-HWK-WK2/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,13 +13,25 @@ namespace BS_HWK_WK2
         static void Main(string[] args)
         {
             int n = 5;
-
             // 非LINQ三角形
-            for (int i = 0; i < n; i++)
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for(int j = 0; j < i + 1; j++)
+            //    {
+            //        Console.Write(n - i);
+            //        //list.Add(n - i);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            IEnumerable<int> numberIList = Enumerable.Range(1, n).Select(x => x);
+            
+            foreach (int i in numberIList.OrderByDescending(x => x))
             {
-                for(int j = 0; j < i + 1; j++)
+                IEnumerable<int> numberJList = Enumerable.Range(1, n - i + 1).Select(x => x);
+                foreach(var j in numberJList)
                 {
-                    Console.Write(n - i);
+                    Console.Write(i);
                 }
                 Console.WriteLine();
             }
